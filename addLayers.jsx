b079d40ffdf8188de0c05eeaa64f742b28e2eb00
xulihang <xulihanghai@163.com>
2023-07-05 21:25:12 +0800

    fix stroke support

--- addLayers.jsx
+++ addLayers.jsx
@@ -189,13 +189,8 @@ function addTextLayer(docRef,layername,X,Y,width,height,text,pfontsize,lineheigh
   }
   
   if (shadowColor!="null"){
-    docRef.selection.selectAll();
-    try {
-      docRef.selection.stroke(getSolidColor(shadowColor),shadowRadius);    
-    } catch (error) {
-
-    }
-    docRef.selection.deselect();
+    var strokeColor = getRGBColor(shadowColor);
+    addStroke(shadowRadius, strokeColor, 100, 'outside');
   }
   
   if (richText==true) {
@@ -389,6 +384,15 @@ function getSolidColor(color){
   return c
 }
 
+function getRGBColor(color){
+  var c = new RGBColor()
+  c.red=color.split(",")[0]
+  c.green=color.split(",")[1]
+  c.blue=color.split(",")[2]
+  return c
+}
+
+
 
 function getJustification(justification){
   justification=parseInt(justification)-1
@@ -472,3 +476,95 @@ function readParams(dirPath){
   }
 }
 
+/* https://community.adobe.com/t5/photoshop-ecosystem-discussions/add-a-stroke-layer-style-effect-to-the-active-layer-extendscript/m-p/8630771#M281723
+
+ * Add Stroke Effect
+
+ * @param {Number} size : 1 - 250
+
+ * @param {Object} color : RGBColor object
+
+ * @param {Number} opacity : 0 - 100
+
+ * @param {Number} position : center / outside / inside
+
+ */
+
+function addStroke(size, color, opacity, position) {
+
+    var strokePosCharID;
+
+    switch(position) {
+
+        case 'center':
+
+            strokePosCharID = 'CtrF';
+
+            break;
+
+        case 'outside':
+
+            strokePosCharID = 'OutF';
+
+            break;
+
+        case 'inside':
+
+            strokePosCharID = 'InsF';
+
+            break;
+
+        default: break; 
+
+    }
+
+
+    var desc = new ActionDescriptor();
+
+    var ref190 = new ActionReference();
+
+    ref190.putProperty( charIDToTypeID( "Prpr" ), charIDToTypeID( "Lefx" ) );
+
+    ref190.putEnumerated( charIDToTypeID( "Lyr " ), charIDToTypeID( "Ordn" ), charIDToTypeID( "Trgt" ) );
+
+    desc.putReference( charIDToTypeID( "null" ), ref190 );
+
+    var fxDesc = new ActionDescriptor();
+
+    var fxPropDesc = new ActionDescriptor();
+
+    fxPropDesc.putBoolean( charIDToTypeID( "enab" ), true );
+
+    fxPropDesc.putBoolean( stringIDToTypeID( "present" ), true );
+
+    fxPropDesc.putBoolean( stringIDToTypeID( "showInDialog" ), true );
+
+    fxPropDesc.putEnumerated( charIDToTypeID( "Styl" ), charIDToTypeID( "FStl" ), charIDToTypeID( strokePosCharID ) );
+
+    fxPropDesc.putEnumerated(  charIDToTypeID( "PntT" ),  charIDToTypeID( "FrFl" ), charIDToTypeID( "SClr" ) );
+
+    fxPropDesc.putEnumerated( charIDToTypeID( "Md  " ), charIDToTypeID( "BlnM" ), charIDToTypeID( "Nrml" ) );
+
+    fxPropDesc.putUnitDouble( charIDToTypeID( "Opct" ), charIDToTypeID( "#Prc" ), opacity );
+
+    fxPropDesc.putUnitDouble( charIDToTypeID( "Sz  " ), charIDToTypeID( "#Pxl") , size );
+
+    var colorDesc = new ActionDescriptor();
+
+    colorDesc.putDouble( charIDToTypeID( "Rd  " ), color.red);
+
+    colorDesc.putDouble( charIDToTypeID( "Grn " ), color.green );
+
+    colorDesc.putDouble( charIDToTypeID( "Bl  " ), color.blue );
+
+    fxPropDesc.putObject( charIDToTypeID( "Clr " ), charIDToTypeID( "RGBC" ), colorDesc );
+
+    fxPropDesc.putBoolean( stringIDToTypeID( "overprint" ), false );
+
+    fxDesc.putObject( charIDToTypeID( "FrFX" ), charIDToTypeID( "FrFX" ), fxPropDesc );
+
+    desc.putObject( charIDToTypeID( "T   " ), charIDToTypeID( "Lefx" ), fxDesc );
+
+    executeAction( charIDToTypeID( "setd" ), desc, DialogModes.NO );
+
+}
